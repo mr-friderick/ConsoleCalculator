79e1bdd5a6d00882e36d7a04bbd4c17de138070a
Pavel Vinogradov <mr.friderick@ya.ru>
2024-01-24 20:05:24 +0300

    Закончены доработки 1 дня

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,21 +1,39 @@
 fun main() {
-    print("Введите число: ")
-    val firstNumber = readln().toDouble()
+    greeting()
 
-    print("Введите знак: ")
-    val symbol = readln()
+    while (true) {
+        print("Введите число: ")
+        val firstNumber = readln().toDouble()
 
-    print("Введите число: ")
-    val secondNumber = readln().toDouble()
+        print("Введите знак: ")
+        val symbol = readln()
 
-    val result =
-    when (symbol) {
+        print("Введите число: ")
+        val secondNumber = readln().toDouble()
+
+        val result = calculate(firstNumber, secondNumber, symbol)
+
+        println("\nРезультат: $result \n")
+
+        println("Продолжаем? y/n")
+        val answer = readln()
+        if (answer == "y") continue else return
+    }
+}
+
+fun greeting() {
+    println("\n#########################################################\n" +
+            "############ Добро пожаловать в калькулятор! ############\n" +
+            "#########################################################\n" +
+            "### Чтобы завершить работу с программой введите 'end' ###\n")
+}
+
+fun calculate(firstNumber: Double, secondNumber: Double, symbol: String): Double {
+    return when (symbol) {
         "+" -> firstNumber + secondNumber
         "-" -> firstNumber - secondNumber
         "*" -> firstNumber * secondNumber
         "/" -> firstNumber / secondNumber
-        else -> 0
+        else -> 0.0
     }
-
-    println("Результат: $result")
 }
